9831eec31b43c933b33019ddc747ec1ca43ac731
Zeliha Usta <zelisstx7@gmail.com>
2025-08-03 22:08:01 +0300

    Level 3 için müzik aletleri soruları eklendi

---
+++ lib/screens/soundplanet/instrument_questions.dart
@@ -0,0 +1,149 @@
+final List<Map<String, dynamic>> instrumentQuestions = [
+  {
+    'sound': 'audio/intruments/piyano.mp3',
+    'correct': 'assets/images/planet2/level3/piyano.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_piyano.mp3',
+    'options': [
+      'assets/images/planet2/level3/piyano.png',
+      'assets/images/planet2/level3/gitar.png',
+      'assets/images/planet2/level3/davul.png',
+    ],
+  },
+  {
+    'sound': 'audio/intruments/cello.mp3',
+    'correct': 'assets/images/planet2/level3/çello.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_çello.mp3',
+    'options': [
+      'assets/images/planet2/level3/gitar.png',
+      'assets/images/planet2/level3/çello.png',
+      'assets/images/planet2/level3/tulum.png',
+    ],
+  },
+
+  {
+    'sound': 'audio/intruments/bateri.mp3',
+    'correct': 'assets/images/planet2/level3/bateri.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_bateri.mp3',
+    'options': [
+      'assets/images/planet2/level3/marakas.png',
+      'assets/images/planet2/level3/bateri.png',
+      'assets/images/planet2/level3/sitar.png',
+    ],
+  },{
+    'sound': 'audio/intruments/gitar.mp3',
+    'correct': 'assets/images/planet2/level3/gitar.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_gitar.mp3',
+    'options': [
+      'assets/images/planet2/level3/gitar.png',
+      'assets/images/planet2/level3/handpan.png',
+      'assets/images/planet2/level3/çello.png',
+    ],
+  },
+  {
+    'sound': 'audio/intruments/kalimba.mp3',
+    'correct': 'assets/images/planet2/level3/kalimba.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_kalimba.mp3',
+    'options': [
+      'assets/images/planet2/level3/kalimba.png',
+      'assets/images/planet2/level3/flüt.png',
+      'assets/images/planet2/level3/ukulele.png',
+    ],
+  },
+
+  {
+    'sound': 'audio/intruments/davul.mp3',
+    'correct': 'assets/images/planet2/level3/davul.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_davul.mp3',
+    'options': [
+      'assets/images/planet2/level3/çello.png',
+      'assets/images/planet2/level3/tulum.png',
+      'assets/images/planet2/level3/davul.png',
+    ],
+  },
+  {
+    'sound': 'audio/intruments/handpan.mp3',
+    'correct': 'assets/images/planet2/level3/handpan.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_handpan.mp3',
+    'options': [
+      'assets/images/planet2/level3/handpan.png',
+      'assets/images/planet2/level3/keman.png',
+      'assets/images/planet2/level3/ukulele.png',
+    ],
+  },
+  {
+    'sound': 'audio/intruments/keman.mp3',
+    'correct': 'assets/images/planet2/level3/keman.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_keman.mp3',
+    'options': [
+      'assets/images/planet2/level3/marakas.png',
+      'assets/images/planet2/level3/keman.png',
+      'assets/images/planet2/level3/flüt.png',
+    ],
+  },
+  {
+    'sound': 'audio/intruments/marakas.mp3',
+    'correct': 'assets/images/planet2/level3/marakas.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_marakas.mp3',
+    'options': [
+      'assets/images/planet2/level3/saksafon.png',
+      'assets/images/planet2/level3/davul.png',
+      'assets/images/planet2/level3/marakas.png',
+    ],
+  },
+  {
+    'sound': 'audio/intruments/saksafon.mp3',
+    'correct': 'assets/images/planet2/level3/saksafon.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_saksafon.mp3',
+    'options': [
+      'assets/images/planet2/level3/bateri.png',
+      'assets/images/planet2/level3/saksafon.png',
+      'assets/images/planet2/level3/flüt.png',
+    ],
+  },
+  {
+    'sound': 'audio/intruments/sitar.mp3',
+    'correct': 'assets/images/planet2/level3/sitar.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_sitar.mp3',
+    'options': [
+      'assets/images/planet2/level3/sitar.png',
+      'assets/images/planet2/level3/keman.png',
+      'assets/images/planet2/level3/ukulele.png',
+    ],
+  },
+  {
+    'sound': 'audio/intruments/tulum.mp3',
+    'correct': 'assets/images/planet2/level3/tulum.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_tulum.mp3',
+    'options': [
+      'assets/images/planet2/level3/flüt.png',
+      'assets/images/planet2/level3/kalimba.png',
+      'assets/images/planet2/level3/tulum.png',
+    ],
+  },
+  {
+    'sound': 'audio/intruments/ukulele.mp3',
+    'correct': 'assets/images/planet2/level3/ukulele.png',
+    'correct_sound': 'audio/tebrikler.mp3',
+    'hint': 'audio/instruments/ipucu_ukulele.mp3',
+    'options': [
+      'assets/images/planet2/level3/handpan.png',
+      'assets/images/planet2/level3/ukulele.png',
+      'assets/images/planet2/level3/sitar.png',
+    ],
+  },
+
+
+
+];
