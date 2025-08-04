09e754a2c4f4152691152161e07df3a6519d76e6
Zeliha Usta <zelisstx7@gmail.com>
2025-08-04 15:11:35 +0300

    Yerel hint ses dosyaları silindi, URL üzerinden ses kullanımı aktif edildi

--- lib/screens/soundplanet/instrument_questions.dart
+++ lib/screens/soundplanet/instrument_questions.dart
@@ -3,7 +3,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/piyano.mp3',
     'correct': 'assets/images/planet2/level3/piyano.png',
     'correct_sound': 'audio/intruments/correct_piyano.mp3',
-    'hint': 'audio/intruments/correct_piyano.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_piyano.mp3',
     'options': [
       'assets/images/planet2/level3/piyano.png',
       'assets/images/planet2/level3/gitar.png',
@@ -14,7 +14,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/cello.mp3',
     'correct': 'assets/images/planet2/level3/çello.png',
     'correct_sound': 'audio/intruments/correct_cello.mp3',
-    'hint': 'audio/intruments/correct.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_cello.mp3',
     'options': [
       'assets/images/planet2/level3/gitar.png',
       'assets/images/planet2/level3/çello.png',
@@ -26,7 +26,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/bateri.mp3',
     'correct': 'assets/images/planet2/level3/bateri.png',
     'correct_sound': 'audio/intruments/correct_bateri.mp3',
-    'hint': 'audio/instruments/ipucu_bateri.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_bateri.mp3',
     'options': [
       'assets/images/planet2/level3/marakas.png',
       'assets/images/planet2/level3/bateri.png',
@@ -36,7 +36,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/gitar.mp3',
     'correct': 'assets/images/planet2/level3/gitar.png',
     'correct_sound': 'audio/intruments/correct_gitar.mp3',
-    'hint': 'audio/instruments/ipucu_gitar.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_gitar.mp3',
     'options': [
       'assets/images/planet2/level3/gitar.png',
       'assets/images/planet2/level3/handpan.png',
@@ -47,7 +47,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/kalimba.mp3',
     'correct': 'assets/images/planet2/level3/kalimba.png',
     'correct_sound': 'audio/intruments/correct_kalimba.mp3',
-    'hint': 'audio/instruments/ipucu_kalimba.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_kalimba.mp3',
     'options': [
       'assets/images/planet2/level3/kalimba.png',
       'assets/images/planet2/level3/flüt.png',
@@ -59,7 +59,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/davul.mp3',
     'correct': 'assets/images/planet2/level3/davul.png',
     'correct_sound': 'audio/intruments/correct_davul.mp3',
-    'hint': 'audio/instruments/ipucu_davul.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_davul.mp3',
     'options': [
       'assets/images/planet2/level3/çello.png',
       'assets/images/planet2/level3/tulum.png',
@@ -70,7 +70,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/handpan.mp3',
     'correct': 'assets/images/planet2/level3/handpan.png',
     'correct_sound': 'audio/intruments/correct_handpan.mp3',
-    'hint': 'audio/instruments/ipucu_handpan.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_handpan.mp3',
     'options': [
       'assets/images/planet2/level3/handpan.png',
       'assets/images/planet2/level3/keman.png',
@@ -81,7 +81,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/keman.mp3',
     'correct': 'assets/images/planet2/level3/keman.png',
     'correct_sound': 'audio/intruments/correct_keman.mp3',
-    'hint': 'audio/instruments/ipucu_keman.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_keman.mp3',
     'options': [
       'assets/images/planet2/level3/marakas.png',
       'assets/images/planet2/level3/keman.png',
@@ -92,7 +92,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/marakas.mp3',
     'correct': 'assets/images/planet2/level3/marakas.png',
     'correct_sound': 'audio/intruments/correct_marakas.mp3',
-    'hint': 'audio/instruments/ipucu_marakas.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_marakas.mp3',
     'options': [
       'assets/images/planet2/level3/saksafon.png',
       'assets/images/planet2/level3/davul.png',
@@ -103,7 +103,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/saksafon.mp3',
     'correct': 'assets/images/planet2/level3/saksafon.png',
     'correct_sound': 'audio/intruments/correct_saksafon.mp3',
-    'hint': 'audio/instruments/ipucu_saksafon.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_saksafon.mp3',
     'options': [
       'assets/images/planet2/level3/bateri.png',
       'assets/images/planet2/level3/saksafon.png',
@@ -114,7 +114,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/sitar.mp3',
     'correct': 'assets/images/planet2/level3/sitar.png',
     'correct_sound': 'audio/intruments/correct_sitar.mp3',
-    'hint': 'audio/intruments/ipucu_sitar.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_sitar.mp3',
     'options': [
       'assets/images/planet2/level3/sitar.png',
       'assets/images/planet2/level3/keman.png',
@@ -125,7 +125,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/tulum.mp3',
     'correct': 'assets/images/planet2/level3/tulum.png',
     'correct_sound': 'audio/intruments/correct_tulum.mp3',
-    'hint': 'audio/instruments/ipucu_tulum.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_tulum.mp3',
     'options': [
       'assets/images/planet2/level3/flüt.png',
       'assets/images/planet2/level3/kalimba.png',
@@ -136,7 +136,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
     'sound': 'audio/intruments/ukulele.mp3',
     'correct': 'assets/images/planet2/level3/ukulele.png',
     'correct_sound': 'audio/intruments/correct_ukulele.mp3',
-    'hint': 'audio/instruments/ipucu_ukulele.mp3',
+    'hint': 'https://zeliha-x0st.github.io/game-assets-sound/hint_ukulele.mp3',
     'options': [
       'assets/images/planet2/level3/handpan.png',
       'assets/images/planet2/level3/ukulele.png',
