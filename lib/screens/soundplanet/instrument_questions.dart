5f43ac7e4b6237348aac082ba2bdd7f97730f025
Zeliha Usta <zelisstx7@gmail.com>
2025-08-04 13:50:12 +0300

    level3 için correct sesleri eklendi

--- lib/screens/soundplanet/instrument_questions.dart
+++ lib/screens/soundplanet/instrument_questions.dart
@@ -2,8 +2,8 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/piyano.mp3',
     'correct': 'assets/images/planet2/level3/piyano.png',
-    'correct_sound': 'audio/tebrikler.mp3',
-    'hint': 'audio/instruments/ipucu_piyano.mp3',
+    'correct_sound': 'audio/intruments/correct_piyano.mp3',
+    'hint': 'audio/intruments/correct_piyano.mp3',
     'options': [
       'assets/images/planet2/level3/piyano.png',
       'assets/images/planet2/level3/gitar.png',
@@ -13,8 +13,8 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/cello.mp3',
     'correct': 'assets/images/planet2/level3/çello.png',
-    'correct_sound': 'audio/tebrikler.mp3',
-    'hint': 'audio/instruments/ipucu_çello.mp3',
+    'correct_sound': 'audio/intruments/correct_cello.mp3',
+    'hint': 'audio/intruments/correct.mp3',
     'options': [
       'assets/images/planet2/level3/gitar.png',
       'assets/images/planet2/level3/çello.png',
@@ -25,7 +25,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/bateri.mp3',
     'correct': 'assets/images/planet2/level3/bateri.png',
-    'correct_sound': 'audio/tebrikler.mp3',
+    'correct_sound': 'audio/intruments/correct_bateri.mp3',
     'hint': 'audio/instruments/ipucu_bateri.mp3',
     'options': [
       'assets/images/planet2/level3/marakas.png',
@@ -35,7 +35,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   },{
     'sound': 'audio/intruments/gitar.mp3',
     'correct': 'assets/images/planet2/level3/gitar.png',
-    'correct_sound': 'audio/tebrikler.mp3',
+    'correct_sound': 'audio/intruments/correct_gitar.mp3',
     'hint': 'audio/instruments/ipucu_gitar.mp3',
     'options': [
       'assets/images/planet2/level3/gitar.png',
@@ -46,7 +46,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/kalimba.mp3',
     'correct': 'assets/images/planet2/level3/kalimba.png',
-    'correct_sound': 'audio/tebrikler.mp3',
+    'correct_sound': 'audio/intruments/correct_kalimba.mp3',
     'hint': 'audio/instruments/ipucu_kalimba.mp3',
     'options': [
       'assets/images/planet2/level3/kalimba.png',
@@ -58,7 +58,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/davul.mp3',
     'correct': 'assets/images/planet2/level3/davul.png',
-    'correct_sound': 'audio/tebrikler.mp3',
+    'correct_sound': 'audio/intruments/correct_davul.mp3',
     'hint': 'audio/instruments/ipucu_davul.mp3',
     'options': [
       'assets/images/planet2/level3/çello.png',
@@ -69,7 +69,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/handpan.mp3',
     'correct': 'assets/images/planet2/level3/handpan.png',
-    'correct_sound': 'audio/tebrikler.mp3',
+    'correct_sound': 'audio/intruments/correct_handpan.mp3',
     'hint': 'audio/instruments/ipucu_handpan.mp3',
     'options': [
       'assets/images/planet2/level3/handpan.png',
@@ -80,7 +80,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/keman.mp3',
     'correct': 'assets/images/planet2/level3/keman.png',
-    'correct_sound': 'audio/tebrikler.mp3',
+    'correct_sound': 'audio/intruments/correct_keman.mp3',
     'hint': 'audio/instruments/ipucu_keman.mp3',
     'options': [
       'assets/images/planet2/level3/marakas.png',
@@ -91,7 +91,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/marakas.mp3',
     'correct': 'assets/images/planet2/level3/marakas.png',
-    'correct_sound': 'audio/tebrikler.mp3',
+    'correct_sound': 'audio/intruments/correct_marakas.mp3',
     'hint': 'audio/instruments/ipucu_marakas.mp3',
     'options': [
       'assets/images/planet2/level3/saksafon.png',
@@ -102,7 +102,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/saksafon.mp3',
     'correct': 'assets/images/planet2/level3/saksafon.png',
-    'correct_sound': 'audio/tebrikler.mp3',
+    'correct_sound': 'audio/intruments/correct_saksafon.mp3',
     'hint': 'audio/instruments/ipucu_saksafon.mp3',
     'options': [
       'assets/images/planet2/level3/bateri.png',
@@ -113,8 +113,8 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/sitar.mp3',
     'correct': 'assets/images/planet2/level3/sitar.png',
-    'correct_sound': 'audio/tebrikler.mp3',
-    'hint': 'audio/instruments/ipucu_sitar.mp3',
+    'correct_sound': 'audio/intruments/correct_sitar.mp3',
+    'hint': 'audio/intruments/ipucu_sitar.mp3',
     'options': [
       'assets/images/planet2/level3/sitar.png',
       'assets/images/planet2/level3/keman.png',
@@ -124,7 +124,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/tulum.mp3',
     'correct': 'assets/images/planet2/level3/tulum.png',
-    'correct_sound': 'audio/tebrikler.mp3',
+    'correct_sound': 'audio/intruments/correct_tulum.mp3',
     'hint': 'audio/instruments/ipucu_tulum.mp3',
     'options': [
       'assets/images/planet2/level3/flüt.png',
@@ -135,7 +135,7 @@ final List<Map<String, dynamic>> instrumentQuestions = [
   {
     'sound': 'audio/intruments/ukulele.mp3',
     'correct': 'assets/images/planet2/level3/ukulele.png',
-    'correct_sound': 'audio/tebrikler.mp3',
+    'correct_sound': 'audio/intruments/correct_ukulele.mp3',
     'hint': 'audio/instruments/ipucu_ukulele.mp3',
     'options': [
       'assets/images/planet2/level3/handpan.png',
